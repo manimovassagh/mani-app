d76dd344518f1ac7fbe27f22fd4ab20c9d886542
Mani Movassagh <mani.mgh@gmail.com>
2024-05-13 19:18:22 +0200

    change

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/jackc/pgx v3.6.2+incompatible
 require (
 	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type Student struct {
+	ID       int    `db:"id"`
+	Name     string `db:"name"`
+	Age      int    `db:"age"`
+	Location string `db:"location"`
+}
+
 func main() {
 	//connect to a PostgreSQL database
 	// Replace the connection details (user, dbname, password, host) with your own
 	db, err := sqlx.Connect("postgres", "user=postgres dbname=mani sslmode=disable password=postgres host=localhost")
+
 	if err != nil {
 		log.Fatalln(err)
 	}
